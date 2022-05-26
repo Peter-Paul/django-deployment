2368ca45723c59dd329656e5213b44785257c56e
unknown <ppmunga@gmail.com>
2022-05-26 22:33:49 +0300

    Added allowed host

--- deployment/deployment/settings.py
+++ deployment/deployment/settings.py
@@ -10,9 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y)0du8cf3d12r*hcxizbicsv2@m_4tp%++ozb97*-%vv!2r7d$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] # Accepts from all origins
+# ALLOWED_HOSTS = ["localhost"] # Accepts only from localhost
 
 
 # Application definition
